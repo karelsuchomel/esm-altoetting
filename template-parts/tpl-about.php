5ffe55450d8e319f376ca7d6693739157f083503
Karel Suchomel <karelsuchomel@windowslive.com>
2018-01-28 23:34:34 +0100

    Added 50% finished About page

--- template-parts/tpl-about.php
+++ template-parts/tpl-about.php
@@ -1,72 +1,113 @@
-<?php /* Template name: About - specific page */ ?>
+<?php /* Template name: About */ ?>
 <?php get_header();?>
 
 <?php require_once('navigation/top-menu-bar.php');?>
 
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/assets/css/tpl-student-werden.css">
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/assets/css/tpl-about.css">
 
-<div id="content" class="student-werden-container">
+<div id="content" class="about-page-container">
 
-<div id="mobile-menu-zoom-el" class="gradient-card std-werden">
+<div id="mobile-menu-zoom-el" class="gradient-card about-page front-card">
 	<div class="card-content">
 	<h1>
-		Student werden
+		About
 	</h1>
 	<p>
-		Ich habe mich nach dem Weltjugendtag 2016 dazu entschieden mich für die ESM zu bewerben, obwohl ich eigentlich den Plan hatte ein Studium zu beginnen. Ich kannte weder die Gemeinschaft Emmanuel noch die ESM wirklich gut und hatte mich erst einige Tage vorher dazu entschieden mein Leben ganz für Jesus leben zu wollen, sodass es eigentlich eine sehr spontane Entscheidung war. Trotzdem war ich mir schnell sicher, dass dieses Jahr für Gott genau das ist, was ich machen möchte. Der Bewerbungsprozess hat mich darin immer mehr bestätigt.<br>
-		Ein Jahr später bin ich sehr froh über diese Entscheidung. Ich schaue zurück auf viele frohe und tiefe Momente in der Gemeinschaft und habe in diesem Jahr vor allem Jesus kennengelernt.
+		Ich habe mich nach dem Weltjugendtag 2016 dazu entschieden mich für die ESM zu bewerben, obwohl ich eigentlich den Plan hatte ein Studium zu beginnen. Ich kannte weder die Gemeinschaft Emmanuel noch die ESM wirklich gut und hatte mich erst einige Tage vorher dazu entschieden mein Leben ganz für Jesus leben zu wollen, sodass es eigentlich eine sehr spontane Entscheidung war.
 	</p>
 	</div>
 </div>
 
-<div class="fyfty-fyfty-text-column-card clear-both">
-	<div class="column-text">
-		<div class="column-icon bus"></div>
-		<h2>
-			Komm zum Besuch
-		</h2>
-		<p>
-			Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht.
-		</p>
-	</div>
-	<div class="column-text">
-		<div class="column-icon cross"></div>
-		<h2>
-			Bete dafur
-		</h2>
-		<p>
-			körperlicher Betätigung, außer um Vorteile daraus zu ziehen? Aber wer hat irgend ein Recht, einen Menschen zu tadeln, der die Entscheidung trifft, eine Freude zu genießen, die keine unangenehmen Folgen hat, oder einen, der Schmerz vermeidet, welcher keine daraus resultierende.
-		</p>
+<div class="two-logos-segment">
+	<div class="two-logos-content">
+		<div class="logos-wrap">
+			<img src="<?php echo bloginfo('template_url') . "/assets/images/logo_esm_altotting_dark.png"; ?>" class="organization-logo">
+			<img src="<?php echo bloginfo('template_url') . "/assets/images/logo_pul_dark.png"; ?>" class="organization-logo">
+		</div>
+		<span>
+			In Kooperation mit Lateran Iniversitat Rom
+		</span>
 	</div>
 </div>
 
-<div class="gradient-card std-werden">
+<div class="gradient-card about-page cross">
+	<div class="card-content">
+	<h1>
+		Gebet
+	</h1>
+	<p>
+		Eine persönliche Beziehung zu Gott entdecken und vertiefen, durch Gebet, Hl. Messe, Stille, Bibellesen, Exerzitien und mehr.
+	</p>
+	</div>
+</div>
+
+<div class="gradient-card about-page text-align-right ship">
+	<div class="card-content">
+	<h1>
+		Gemeinschaft
+	</h1>
+	<p>
+		Junge Erwachsene aus vielen verschiedenen Nationen leben gemeinsam in der Josefsburg – eine Schule der Nächstenliebe. 
+	</p>
+	</div>
+</div>
+
+<div class="gradient-card about-page book">
+	<div class="card-content">
+	<h1>
+		Unterricht
+	</h1>
+	<p>
+		Durch eine menschliche, theologische und philosophische Ausbildung, sich selbst und den Glauben besser kennenlernen. 
+	</p>
+	</div>
+</div>
+
+<div class="gradient-card about-page text-align-right church">
+	<div class="card-content">
+	<h1>
+		Mission
+	</h1>
+	<p>
+		Die Liebe und Barmherzigkeit Gottes für sich selbst entdecken und lernen, sie in Freude an andere Menschen weiterzugeben. 
+	</p>
+	</div>
+</div>
+
+
+<div class="gradient-card about-page josefsburg-card">
+	<div class="card-content">
+	<h1>
+		Josefsburg
+	</h1>
+	<p>
+		Ich habe mich nach dem Weltjugendtag 2016 dazu entschieden mich für die ESM zu bewerben, obwohl ich eigentlich den Plan hatte ein Studium zu beginnen. Ich kannte weder die Gemeinschaft Emmanuel noch die ESM wirklich gut und hatte mich erst einige.
+	</div>
+</div>
+
+<div class="newsletter-subscribe-card">
 	<div class="card-content">
 	<p>
-		Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen<br> Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel.daraus zu ziehen? Aber wer hat irgend ein Recht, einen Menschen zu tadeln, der die Entscheidung trifft, eine Freude zu genießen, die keine unangenehmen Folgen hat, oder einen, der Schmerz vermeidet, welcher keine daraus resultierende Freude nach sich zieht?Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur.
+		Wir verschicken den Newsletter dreimal pro Jahr. Lass dir die Neuigkeiten per E-mail schicken lassen.
 	</p>
+	<form type="POST">
+		<input id="field" type="text" name="emailAddress" placeholder="deine E-mail Adresse" required>
+		<input id="button" type="submit" value="News per E-mail bekommen">
+	</form>
 	</div>
 </div>
 
-<div class="headline-text-two-column-card clear-both">
-	<div class="column-text">
-		<h2>
-			Aber,<br class="hide-on-small-screen"> ist das alles<br class="hide-on-small-screen"> fur mich?
-		</h2>
+<div class="two-boxes">
+	<div class="box interessiere">
+		<h2>Ich interessiere mich für die ESM</h2>
+		<p>Super – wir wollen dich kennenlernen! Schreib uns ein E-Mail und wir vereinbaren einen Termin um miteinander zu sprechen und deine Fragen zu beantworten. Nur Mut, wir freuen uns auf dich!</p>
+		<a href="mailto:&#098;&#101;&#119;&#101;&#114;&#098;&#117;&#110;&#103;&#064;&#101;&#115;&#109;&#045;&#097;&#108;&#116;&#111;&#101;&#116;&#116;&#105;&#110;&#103;&#046;&#100;&#101;" class="two-boxes-button">&#098;&#101;&#119;&#101;&#114;&#098;&#117;&#110;&#103;&#064;&#101;&#115;&#109;&#045;&#097;&#108;&#116;&#111;&#101;&#116;&#116;&#105;&#110;&#103;&#046;&#100;&#101;</a>
 	</div>
-	<div class="column-text">
-		<p>
-			körperlicher Betätigung, außer um Vorteile daraus zu ziehen? Aber wer hat irgend ein Recht, einen Menschen zu tadeln, der die Entscheidung trifft, eine Freude zu genießen, die keine unangenehmen Folgen hat, oder einen, der Schmerz vermeidet, welcher keine daraus resultierende.<br>
-			körperlicher Betätigung, außer um Vorteile daraus zu ziehen? Aber wer hat irgend ein Recht, einen Menschen zu tadeln, der die Entscheidung trifft, eine Freude zu genießen, die keine unangenehmen Folgen hat, oder einen, der Schmerz vermeidet, welcher keine daraus resultierende.
-		</p>
+	<div class="box blue">
+		<h2>Ich möchte die ESM unterstützen</h2>
+		<p>Herzlichen Dank – wir freuen uns über jede Hilfe, ob klein oder groß. Unsere Arbeit finanziert sich fast ausschließlich von Spenden und Patenschaften. Wir sind dankbar für jegliche Unterstützung.</p>
+		<a href="mailto:&#100;&#105;&#114;&#101;&#107;&#116;&#111;&#114;&#064;&#101;&#115;&#109;&#045;&#097;&#108;&#116;&#111;&#101;&#116;&#116;&#105;&#110;&#103;&#046;&#100;&#101;" class="two-boxes-button">&#100;&#105;&#114;&#101;&#107;&#116;&#111;&#114;&#064;&#101;&#115;&#109;&#045;&#097;&#108;&#116;&#111;&#101;&#116;&#116;&#105;&#110;&#103;&#046;&#100;&#101;</a>
 	</div>
-</div>
-
-<div class="hand-mail-card">
-	<a href="mailto:bewerbung@esm-altoetting.de">
-		bewerbung@esm-altoetting.de
-		<div class="hand-button-icon"></div>
-	</a>
 </div>
 
 <?php get_footer();?>
